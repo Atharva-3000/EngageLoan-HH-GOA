984c7c2b7293f015784599bc25693e02b6891e1b
Atharva_404 <72994819+Atharva-3000@users.noreply.github.com>
2024-08-06 18:53:05 +0530

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -10,7 +10,7 @@ paths: {
 networks: {
     polygonAmoy: {
     url: "https://polygon-amoy.g.alchemy.com/v2/GDPZGLVRyXfR8mvXf5xi80OiOJx8oqX8",
-    accounts: a5b744938d96120d79f094828ee4e5ae6a84a8e40c0a5154a39e09eb8e00a113,
+    accounts: ,
     chainId:80002,
     },
 },
