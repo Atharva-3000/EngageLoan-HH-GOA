961293681ee453776819633875b9d59b9d065ed9
Atharva <deshmukhsambhajirao9@gmail.com>
2024-08-07 00:41:29 +0530

    chore: Remove private key from hardhat.config.js

--- hardhat/hardhat.config.cjs
+++ hardhat/hardhat.config.cjs
@@ -31,7 +31,7 @@ module.exports = {
     networks: {
         polygonAmoy: {
             url: "https://polygon-amoy.g.alchemy.com/v2/GDPZGLVRyXfR8mvXf5xi80OiOJx8oqX8",
-            accounts: ["a5b744938d96120d79f094828ee4e5ae6a84a8e40c0a5154a39e09eb8e00a113"], // Use environment variable for the private key
+            accounts: [""], // Use environment variable for the private key
             chainId: 80002,
         },
     },
